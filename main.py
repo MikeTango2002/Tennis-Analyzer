2b2fc49ad8f7fd1242cd7f53bff10b6e533e3e18
MikeTango2002 <miketango2002@gmail.com>
2024-06-01 18:58:18 +0200

    Aggiunto il main

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ from trackers import PlayerTracker
 
 
 def main():
-    #Read Video
+    # Read Video
     input_video_path = "input_videos/input.mp4"
     video_frames = read_video(input_video_path)
 
-    #Detect Players
+    # Detect Players
     player_tracker = PlayerTracker(model_path='models/yolov8x.pt')
     player_detections = player_tracker.detect_multiple_frames(video_frames)
 
-    #Draw output
+    # Draw output
 
-    ##Draw Player Bounding Boxes
+    ## Draw Player Bounding Boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
 
 
-    save_video(video_frames, "output_videos/output.avi")
+    save_video(output_video_frames, "output_videos/output.avi")
+    
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
